41d6c3a2ecaec62c2ac8c47c22a3b7587f60f162
michael <michaelglenn88@yahoo.com>
2022-03-18 22:25:19 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(name) {
+  let newArr = [];
+  let nam = 0;
+  for (let i = 0; i < name.length; i++) {
+    nam = `Thank you, ` + name[i] + `, for the wonderful surprise gift!`;
+    newArr.push(nam);
+  }
+
+  return newArr;
+}
+
+function countDown(num) {
+  for (let j = 0; j <= num; num--) {
+    console.log(num);
+    // debugger;
+  }
+}
